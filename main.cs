f7df01c80d79bbc942e34b48828816573ce83bd7
Alex Olhovskiy <alexmokhoff@ukr.net>
2023-03-23 18:38:09 +0200

    Start after misstake

---
+++ main.cs
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Введите имя");
+    String username=Console.ReadLine();
+    Console.WriteLine(username);
+  }
+}
